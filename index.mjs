650f7b476ad82bcc043061abeffb14915569620f
danydev <atriasoft.ceo@gmail.com>
2025-05-16 09:13:45 -0500

    Version estable 1.1

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 
 dotenv.config();
@@ -9,12 +10,18 @@ import syncRoutes from './routes/sync.routes.mjs';
 import createRoutes from './routes/create.routes.mjs';
 import updateDirectRoutes from './controllers/update-direct.controller.mjs';
 
-
-
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // frontend
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions));
+
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use('/auth', authRoutes);
